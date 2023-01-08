a35539a872fdcfd0c32cb2fc01cd7e6d41099d63
AlehMicio <doynickow.oleg@yandex.ru>
2023-01-08 16:44:12 +0300

    Clean

--- app/src/main/java/android/redcall/service/CallReceiver.java
+++ app/src/main/java/android/redcall/service/CallReceiver.java
@@ -14,7 +14,6 @@ import android.widget.Toast;
 
 public class CallReceiver extends BroadcastReceiver {
 
-    //private TelephonyManager telephonyManager;
     private Context mContext;
     private static int mState;
     public static boolean isActive;
@@ -25,92 +24,21 @@ public class CallReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
 
-       /* telephonyManager = (TelephonyManager) context.getSystemService(Context.TELEPHONY_SERVICE);
-        if (stateListener == null) {
-            stateListener = new StateListener();
-            telephonyManager.listen(stateListener, PhoneStateListener.LISTEN_CALL_STATE);
-            mContext = context;
-        } */
-
-        //Так всё работает (если не заработает - то закоменть код с вызовом сервиса):
         String whatACall = intent.getAction();
-        //telephonyManager = (TelephonyManager) context.getSystemService(Context.TELEPHONY_SERVICE);
         if (isActive) {
             if (whatACall != null) {
-                //nameNum1 = intent.getExtras().getString("android.intent.extra.PHONE_NUMBER");
                 if (intent.hasExtra(TelephonyManager.EXTRA_INCOMING_NUMBER)) {
                     nameNum = intent.getStringExtra(TelephonyManager.EXTRA_INCOMING_NUMBER);
                     //nameNum = checkToContact(intent.getStringExtra(TelephonyManager.EXTRA_INCOMING_NUMBER));
                     if (stateListener == null) {
                         stateListener = new StateListener();
-                        //telephonyManager.listen(stateListener, PhoneStateListener.LISTEN_CALL_STATE);
-                        // String state = intent.getStringExtra("state");
                         stateListener.callState(context, intent, nameNum);
                         mContext = context;
                     }
                 }
-
             }
         }
-
-       /* String whatACall = intent.getAction();
-        if ("android.intent.action.PHONE_STATE".equals(whatACall)){
-
-        }
-
-        if ("android.intent.action.NEW_OUTGOING_CALL".equals(whatACall)){
-
-        } */
-
     }
-
-     /*public class StateListener extends PhoneStateListener{
-        @Override
-        public void onCallStateChanged(int state, String phoneNumber) {
-            super.onCallStateChanged(state, phoneNumber);
-
-            String nameNum;
-
-            if(phoneNumber != null && phoneNumber.length() > 0){
-                //nameNum = phoneNumber;
-                //nameNum = checkToContact(phoneNumber);
-            }
-
-            if (nameNum1 != null){
-                nameNum = nameNum1;
-            }
-            else nameNum = "name";
-
-            switch (state){
-                case TelephonyManager.CALL_STATE_RINGING:
-                    //Входящий звонок
-                    mState = 1;
-                    break;
-
-                case TelephonyManager.CALL_STATE_OFFHOOK:
-                    //Идёт телефонный разговор
-                    mState = 2;
-                    Intent intent = new Intent(mContext, BackgroundService.class);
-                    intent.putExtra("name", nameNum);
-                    mContext.startService(intent);
-                    Toast.makeText(mContext, "Идёт запись", Toast.LENGTH_SHORT).show();
-                    break;
-
-                case TelephonyManager.CALL_STATE_IDLE:
-                    if (mState == 2){
-                        //Вызов, который был принят и завершён
-                        mState = 0;
-                        mContext.stopService(new Intent(mContext, BackgroundService.class));
-                        Toast.makeText(mContext, "Запись остановлена", Toast.LENGTH_SHORT).show();
-                    }
-                    if (mState == 1){
-                        //Вызов, который был отклонён
-                        mState = 0;
-                    }
-                    break;
-            }
-        }
-    } */
 
     public class StateListener {
         public void callState(Context context, Intent intent, String nameNum) {
@@ -119,7 +47,6 @@ public class CallReceiver extends BroadcastReceiver {
                 case "RINGING":
                     //Входящий звонок
                     mState = 1;
-                    //Toast.makeText(context, "" + mState, Toast.LENGTH_SHORT).show();
                     break;
 
                 case "OFFHOOK":
@@ -129,7 +56,6 @@ public class CallReceiver extends BroadcastReceiver {
                     context.startService(intentCall);
                     Toast.makeText(context, "Идёт запись", Toast.LENGTH_SHORT).show();
                     mState = 2;
-                    //Toast.makeText(context, "" + mState, Toast.LENGTH_SHORT).show();
                     break;
 
                 case "IDLE":
@@ -138,18 +64,15 @@ public class CallReceiver extends BroadcastReceiver {
                         mState = 0;
                         context.stopService(new Intent(context, BackgroundService.class));
                         Toast.makeText(context, "Запись остановлена", Toast.LENGTH_SHORT).show();
-                        //Toast.makeText(context, "Зав " + mState, Toast.LENGTH_SHORT).show();
                     }
                     if (mState == 1) {
                         //Вызов, который был отклонён
                         mState = 0;
-                        //Toast.makeText(context, "Отк " + mState, Toast.LENGTH_SHORT).show();
                     }
                     break;
             }
         }
     }
-
 
     @SuppressLint("Range")
     private String checkToContact(String phoneNumber){
